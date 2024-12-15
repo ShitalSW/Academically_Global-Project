080ff94695341ff84adab45ada18f5a05052a560
ShitalSW <162702014+ShitalSW@users.noreply.github.com>
2024-12-16 01:35:10 +0530

    Add files via upload
    Backend Components

---
+++ About.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+const About=()=>{
+    return(
+        <div className="about">
+        <h2>An Integrated
+        Platform for Healthcare Professionals and Students</h2>
+        <p>We provide comprehensive resources and guidance to healthcare professionals seeking to migrate 
+        <p>abroad for career enhancement,assisting them in their journey.</p>
+        <p>We take pride in setting a benchmark in the education industry by 
+        offering one-of-a-kind courses to aspirants</p> <p>and students.
+        
+         Our courses and services are unique and best-in-class in the healthcare and education industry,
+         </p> and we offer guidance like no other.</p>
+        
+        <p>Based in Sydney, Australia, the team Academically engages with global healthcare educators,
+         healthcare industry experts, universities, 
+        <p>and higher education institutions to provide demanding healthcare 
+        </p>courses for global audiences.</p>
+
+        <div className="about"> 
+            <p><h1>Our Mission</h1>
+The team of Academically is driven by the goal of helping qualified healthcare professionals across the globe achieve their passions and dreams.
+
+The expert faculty at Academically possess immense experience, which they aim to impart to the aspirants. We desire to make the process of establishing a great career in different countries easier for medical professionals.
+
+We aim to educate and train pharmacists, medical doctors, nurses, lab technicians, physiotherapists, and so on to help them acquire the necessary skills and competencies to register as certified healthcare professionals worldwide.
+
+<h2>Our Vision</h2>
+Dr. Akram Ahmad, our founder, had a vision in mind to impart valuable and undiscovered facts about building a career in the healthcare and medical industry to interested candidates.
+
+Over the years, our founder and the whole team of Academically have gained immense knowledge and understanding about building a career in healthcare from scratch, which they have a vision of letting all the aspirants know.
+
+We, therefore, stand before you, guiding you with the unexplored and unfamiliar aspects of healthcare career opportunities in Australia and all other countries, so that you can make the first step and carry on your process to success.</p>
+        </div>
+        </div>
+    )
+}
+
+export default About;
