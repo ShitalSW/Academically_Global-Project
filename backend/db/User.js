b0b752d9d7a2639359ce3e45086b0721ba8b7dae
ShitalSW <wasadeshital310@gmail.com>
2024-12-16 20:29:11 +0530

    Delete

--- backend/db/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require('mongoose');
-//model for Users Collection
-const userSchema= new mongoose.Schema({
-    name:String,
-    email:String,
-    password:String
-});
-
-module.exports=mongoose.model("user",userSchema);
--- backend/db/config.js
+++
@@ -1,2 +0,0 @@
-const mongoose= require('mongoose');
-mongoose.connect("mongodb://localhost:27017/AcademicallyGlobal");//create connection with database
